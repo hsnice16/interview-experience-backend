4bab14af8b081a6ea60b5c5084d6ffdd6859a246
Himanshu Singh <hsnice16@gmail.com>
2023-10-15 13:28:19 +0530

    feat: mutations query to add a new Blog and update Blog status (#3)
    Co-authored-by: Himanshu Singh <himanshusingh@Himanshus-MacBook-Air.local>

--- src/db/organisations.ts
+++ src/db/organisations.ts
@@ -1,26 +1,26 @@
 export const organisations = [
   {
-    _id: "c001",
+    _id: "org_000",
     name: "Google",
     blogCount: 2,
   },
   {
-    _id: "c002",
+    _id: "org_001",
     name: "Amazon",
     blogCount: 1,
   },
   {
-    _id: "c003",
+    _id: "org_002",
     name: "Atlassian",
     blogCount: 1,
   },
   {
-    _id: "c004",
+    _id: "org_003",
     name: "FactSet",
     blogCount: 1,
   },
   {
-    _id: "c005",
+    _id: "org_004",
     name: "Razorpay",
     blogCount: 1,
   },
--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,6 +1,7 @@
 import { GraphQLError } from "graphql";
 import { blogs } from "./db/blogs";
 import { organisations } from "./db/organisations";
+import { stagingBlogs } from "./db/stagingBlogs";
 
 export enum CustomErrorCodes {
   BAD_USER_INPUT = "BAD_USER_INPUT",
@@ -39,7 +40,9 @@ export const resolvers = {
     blogs: function (_, args) {
       if (args.filter?.forOrganisation) {
         return blogs.filter(
-          (blog) => blog.forOrganisation === args.filter.forOrganisation
+          (blog) =>
+            blog.forOrganisation.toLowerCase() ===
+            args.filter.forOrganisation.toLowerCase()
         );
       }
 
@@ -54,6 +57,109 @@ export const resolvers = {
       checkOffsetValue(args.offset);
 
       return organisations.slice(args.offset, args.offset + args.limit);
+    },
+
+    stagingBlogs: function (_, args) {
+      return stagingBlogs.filter(
+        (blog) => blog.status.toLowerCase() === args.status
+      );
+    },
+  },
+
+  Mutation: {
+    createBlog: function (_, args) {
+      const {
+        title,
+        link,
+        forOrganisation,
+        author: { name, profile },
+      } = args;
+
+      if (
+        blogs.some(
+          (blog) =>
+            blog.author.name === name &&
+            blog.author.profile === profile &&
+            blog.forOrganisation.toLowerCase() ===
+              forOrganisation.toLowerCase() &&
+            blog.link === link &&
+            blog.title === title
+        )
+      ) {
+        throw new GraphQLError(errorMsgs[CustomErrorCodes.BAD_USER_INPUT], {
+          extensions: {
+            code: CustomErrorCodes.BAD_USER_INPUT,
+            argumentName:
+              "title, link, forOrganisation, author.name, author.profile",
+            reason: "blog with same info already exists",
+          },
+        });
+      }
+
+      const newBlog = {
+        _id: `new_blog_00${stagingBlogs.length}`,
+        title,
+        link,
+        forOrganisation,
+        author: { name, profile },
+        status: "pending",
+      };
+
+      stagingBlogs.push(newBlog);
+      return newBlog;
+    },
+
+    updateBlogStatus: function (_, args) {
+      if (stagingBlogs.every((blog) => blog._id !== args._id)) {
+        throw new GraphQLError(errorMsgs[CustomErrorCodes.BAD_USER_INPUT], {
+          extensions: {
+            code: CustomErrorCodes.BAD_USER_INPUT,
+            argumentName: "_id",
+            reason: "no blog with the given _id exists",
+          },
+        });
+      }
+
+      if (
+        !["approved", "rejected"].includes(args.status.toLowerCase().trim())
+      ) {
+        throw new GraphQLError(errorMsgs[CustomErrorCodes.BAD_USER_INPUT], {
+          extensions: {
+            code: CustomErrorCodes.BAD_USER_INPUT,
+            argumentName: "status",
+            reason: "status can be approved or rejected only",
+          },
+        });
+      }
+
+      if (args.status === "rejected") {
+        const blog = stagingBlogs.find((blog) => blog._id === args._id);
+        blog.status = "rejected";
+
+        return blog;
+      }
+
+      const blogIndex = stagingBlogs.findIndex((blog) => blog._id === args._id);
+      const blog = stagingBlogs.splice(blogIndex, 1)[0];
+      const _blog = { ...blog, status: "approved" };
+      delete blog.status;
+      blogs.push({ ...blog, _id: `blog_00${blogs.length}` });
+
+      const organisation = organisations.find(
+        (org) => org.name.toLowerCase() === blog.forOrganisation.toLowerCase()
+      );
+
+      if (organisation) {
+        organisation.blogCount += 1;
+      } else {
+        organisations.push({
+          _id: `org_00${organisations.length}`,
+          name: blog.forOrganisation,
+          blogCount: 1,
+        });
+      }
+
+      return _blog;
     },
   },
 };
